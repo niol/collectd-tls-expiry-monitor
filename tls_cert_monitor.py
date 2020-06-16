8f0614d7db9266789e062b2e827d117979208f07
Joris Baum <joris@audriga.com>
2020-06-16 14:23:03 +0200

    Use pylint

--- tls_cert_monitor.py
+++ tls_cert_monitor.py
@@ -1,14 +1,13 @@
+"""Retrieves the expiry of certificates from a list of hosts"""
 import datetime
 import socket
-import collectd
 import ssl
+
+import collectd
+
 
 PLUGIN_NAME = 'tls-cert-monitor'
 INTERVAL = 10  # seconds
-
-_hosts = None
-
-collectd.info('tls-cert-monitor: Loading Python plugin: ' + PLUGIN_NAME)
 
 
 def configure(configobj):
@@ -28,7 +27,6 @@ def configure(configobj):
         </Module>
     </Plugin>
     '''
-
     global _hosts
 
     collectd.info(
@@ -36,26 +34,28 @@ def configure(configobj):
         (configobj.key, configobj.children))
 
     config = {c.key: c.values for c in configobj.children}
+
     collectd.info('tls-cert-monitor: Configured with %r' % (config))
 
     # Set a module-global based on external configuration
     _hosts = config.get('hosts')
-    # _domains = next(iter(config.get('hosts')))
 
 
 def ssl_expiry_datetime(hostname):
-    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
-
+    """Get expiry date of cert's."""
     context = ssl.create_default_context()
     conn = context.wrap_socket(
         socket.socket(socket.AF_INET),
         server_hostname=hostname,
     )
+
     # 3 second timeout
     conn.settimeout(3.0)
-
     conn.connect((hostname, 443))
+
     ssl_info = conn.getpeercert()
+    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+
     # parse the string from the certificate into a Python datetime object
     return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
 
@@ -69,20 +69,28 @@ def ssl_valid_time_remaining(hostname):
     return expires - datetime.datetime.utcnow()
 
 
+# TODO remove data?
 def read(data=None):
+    """Export number of days left to collectd."""
     for host in _hosts:
-        r = ssl_valid_time_remaining(host)
-        r = r.total_seconds()
-        r = int(r)
+        remaining = ssl_valid_time_remaining(host)
+        remaining = remaining.total_seconds()
+        remaining = int(remaining)
 
         collectd.info(
             'tls-cert-monitor(host=%s): Reading data (data=%d)' %
-            (host, r))
+            (host, remaining))
 
         val = collectd.Values(type='gauge', type_instance=host)
+
         val.plugin = 'tls-cert-monitor'
-        val.dispatch(values=[r])
+        val.dispatch(values=[remaining])
 
 
+# Use a global variable here because we love python
+# pylint: disable=C0103,W0603
+_hosts = None
+
+collectd.info('tls-cert-monitor: Loading Python plugin: ' + PLUGIN_NAME)
 collectd.register_config(configure)
 collectd.register_read(read, INTERVAL)
